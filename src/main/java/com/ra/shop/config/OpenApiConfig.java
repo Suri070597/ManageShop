dbb0f21d0c047ed68138d8daabfddc5c7ad89232
Nguyễn Thị Kim Soàn <soanntkce180197@fpt.edu.vn>
2025-09-26 09:26:08 +0700

    Cập nhật OpenAPI config để Swagger tự động gắn JWT

--- src/main/java/com/ra/shop/config/OpenApiConfig.java
+++ src/main/java/com/ra/shop/config/OpenApiConfig.java
@@ -1,13 +1,25 @@
 package com.ra.shop.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
